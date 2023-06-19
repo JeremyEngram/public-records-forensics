3bf563d02a083a9a6c29eefcd30c1e6e55a111cc
JeremyEngram <65774401+JeremyEngram@users.noreply.github.com>
2023-06-19 18:44:18 -0400

    Update start-public-records-forensics.sh

--- start-public-records-forensics.sh
+++ start-public-records-forensics.sh
@@ -2,12 +2,21 @@
 
 # User-friendly script for public records forensics
 
+LOG_FILE="public_records.log"
+
+# Function to log messages
+log_message() {
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] $1" >> "$LOG_FILE"
+}
+
 # Function to search court records
 search_court_records() {
     read -p "Enter the keyword to search in court records: " keyword
     read -p "Enter the file name of court records: " filename
-    echo "Searching court records..."
+    log_message "Searching court records for keyword: $keyword"
     grep "$keyword" "$filename"
+    log_message "Search in court records complete."
     read -p "Press Enter to continue..."
 }
 
@@ -15,61 +24,4 @@ search_court_records() {
 search_property_records() {
     read -p "Enter the keyword to search in property records: " keyword
     read -p "Enter the file name of property records: " filename
-    echo "Searching property records..."
-    awk -F',' -v pattern="$keyword" '$0 ~ pattern {print $1,$3}' "$filename"
-    read -p "Press Enter to continue..."
-}
-
-# Function to search business registrations
-search_business_registrations() {
-    read -p "Enter the URL of the business registrations API: " url
-    echo "Searching business registrations..."
-    curl "$url"
-    read -p "Press Enter to continue..."
-}
-
-# Function to search government databases
-search_government_databases() {
-    read -p "Enter the URL of the government database: " url
-    echo "Searching government databases..."
-    wget -O temp_file "$url"
-    file temp_file
-    strings temp_file
-    rm temp_file
-    read -p "Press Enter to continue..."
-}
-
-# Main menu
-while true; do
-    clear
-    echo "----- Public Records Forensics -----"
-    echo "1. Search court records"
-    echo "2. Search property records"
-    echo "3. Search business registrations"
-    echo "4. Search government databases"
-    echo "0. Exit"
-
-    read -p "Please select an option: " choice
-
-    case $choice in
-        1)
-            search_court_records
-            ;;
-        2)
-            search_property_records
-            ;;
-        3)
-            search_business_registrations
-            ;;
-        4)
-            search_government_databases
-            ;;
-        0)
-            echo "Exiting..."
-            exit 0
-            ;;
-        *)
-            echo "Invalid option. Please try again."
-            ;;
-    esac
-done
+    log_message "Searching property records for keyword: $keyword
